ca00847ad163e8719d4bde299ebfa00ae2f62e17
kyarnk <127040337+kyarnk@users.noreply.github.com>
2025-03-14 21:50:27 +0300

    Update runSemgrepScan.groovy return results

--- vars/runSemgrepScan.groovy
+++ vars/runSemgrepScan.groovy
@@ -1,8 +1,10 @@
 // vars/runSemgrepScan.groovy
 def call() {
     echo 'Running Semgrep Scan'
-    // Устанавливаем Semgrep и запускаем сканирование
+    
     SemgrepUtils.installSemgrep('dvna')
-    SemgrepUtils.runSemgrep('dvna', 'auto', 'semgrep-results.json')
-    return readFile('semgrep-results.json')  // Возвращаем результаты сканирования
+    def semgrepResults = SemgrepUtils.runSemgrep('dvna', '--config=auto')
+
+    writeFile file: 'semgrep-results.json', text: semgrepResults
+    return semgrepResults
 }
